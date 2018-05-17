54c862b1b476629ee2007dc759a08ef347052ef7
cwallermedia <admin@cwallermedia.co.uk>
2018-05-17 14:36:39 +0100

    Payment step

---
+++ partial_views/payment.php
@@ -0,0 +1,95 @@
+<div class="row">
+	<div class="col-12">
+		<p class="step-title">Step 6</p>
+		<p class="step-section-title">Confirm &amp; Payment</p>
+		<p>Please select an option as to how you would like to pay for your holiday. Either the full balance of the holiday or pay a small deposit contribution today and pay the rest at a later date.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <p class="form-section-title">Payment Method</p>
+    </div>
+    <div class="col-3 payment-method-options">
+        <input type="radio" name="paymentMethod" id="payDeposit" checked="checked"><label for="payDeposit">Pay deposit only</label><br>
+        <input type="radio" name="paymentMethod" id="payFull"><label for="payFull">Pay balance in full</label>
+    </div>
+    <div class="col-6">
+        <label>Amount to Pay</label>
+        <input type="text" value="£1,000,000">
+    </div>
+    <div class="col-3">
+        <img src="https://www.shearings.com/Content/Images/sh/img/safe-payments.png" width="100%">
+    </div>
+</div>
+
+<div class="group">
+    <div class="row">
+        <div class="col-12">
+            <p class="form-section-title">How would you like to pay?</p>
+            <p>There is no fee for credit or debit card payments.</p>
+        </div>
+        <div class="col-sm-4 col-md-2 text-center">
+            <img src="https://s3-eu-west-1.amazonaws.com/shearings-holidays-media/Images/amex4ab081695974.gif?sfvrsn=2" alt="">
+            <input type="radio" name="card" id="">
+        </div>
+        <div class="col-sm-4 col-md-2 text-center">
+            <img src="https://s3-eu-west-1.amazonaws.com/shearings-holidays-media/Images/maestro.gif?sfvrsn=2" alt="">
+            <input type="radio" name="card" id="">
+        </div>
+        <div class="col-sm-4 col-md-2 text-center">
+            <img src="https://s3-eu-west-1.amazonaws.com/shearings-holidays-media/Images/mastercard.gif?sfvrsn=2" alt="">
+            <input type="radio" name="card" id="">
+        </div>
+        <div class="col-sm-4 col-md-2 text-center">
+            <img src="https://s3-eu-west-1.amazonaws.com/shearings-holidays-media/Images/mastercarddebit.gif?sfvrsn=2" alt="">
+            <input type="radio" name="card" id="">
+        </div>
+        <div class="col-sm-4 col-md-2 text-center">
+            <img src="https://s3-eu-west-1.amazonaws.com/shearings-holidays-media/Images/visacredit.gif?sfvrsn=2" alt="">
+            <input type="radio" name="card" id="">
+        </div>
+        <div class="col-sm-4 col-md-2 text-center">
+            <img src="https://s3-eu-west-1.amazonaws.com/shearings-holidays-media/Images/visadebit.gif?sfvrsn=2" alt="">
+            <input type="radio" name="card" id="">
+        </div>
+    </div>
+</div>
+
+<div class="group">
+    <div class="row">
+        <div class="col-12">
+            <p class="form-section-title">Cardholder Details</p>
+        </div>
+    </div>
+    <div class="row">
+       <div class="col-6">
+           <label for="">Title <span class="required">*</span></label>
+           <input type="text">
+           <label for="">First Name <span class="required">*</span></label>
+           <input type="text">
+           <label for="">Surname <span class="required">*</span></label>
+           <input type="text">
+           <label for="">Phone Number <span class="required">*</span></label>
+           <input type="text">
+       </div>
+       <div class="col-6">
+           <p>Billing Address</p>
+           <p>Waterside House<br>Waterside Drive<br>Wigan<br>WN3 5AZ</p>
+       </div>
+    </div>
+</div>
+<div class="group">
+    <div class="row">
+        <div class="col-12">
+        <p class="form-section-title">Communication Preferences</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <p>We’d love to send you details of offers, products and the latest information. We’ll always treat your personal detail with the upmost care and will never sell them to other companies for marketing purposes.</p>
+            <input type="checkbox"> <label>Please tick here if you wish to be kept up to date on our fantastic offers, news and updates by email</label>
+            <input type="checkbox"> <label>I agree to the <a href="#">Privacy Policy</a> and <a href="#">Terms and conditions</a></label>
+        </div>
+    </div>
+</div>
