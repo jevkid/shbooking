f9dc9d1830cd38c5ed3ed665412ddd40f6f32867
cwallermedia <admin@cwallermedia.co.uk>
2018-05-17 10:30:47 +0100

    air and guest details

---
+++ partial_views/guestdetails.php
@@ -0,0 +1,102 @@
+<div class="row">
+	<div class="col-12">
+		<p class="step-title">Step 4</p>
+		<p class="step-section-title">Guest Details</p>
+		<p>Please let us know the names of each member of your party.</p>
+	</div>
+	<div class="col-12">
+		<section data-id="groupone">
+			<div class="section-header">
+				<div class="row">
+					<div class="col-6">1st Group / Room</div>
+					<div class="col-6 text-right">2 Adults</div>
+				</div>
+			</div>
+
+			<div class="row form-row">
+				<div class="col-2">
+					<label for="">Title</label>
+					<select name="" id="">
+						<option value="">-</option>
+						<option value="Mr">Mr</option>
+						<option value="Mrs">Mrs</option>
+						<option value="Miss">Miss</option>
+						<option value="Sir">Sir</option>
+						<option value="Dr">Dr</option>
+						<option value="Fr">Fr</option>
+					</select>
+				</div>
+				<div class="col-5">
+					<label>Forename</label>
+					<input type="text">
+				</div>
+				<div class="col-5">
+					<label>Surname</label>
+					<input type="text">
+				</div>
+			</div>
+
+			<div class="row form-row">
+				<div class="col-2">
+					<label for="">Title</label>
+					<select name="" id="">
+						<option value="">-</option>
+						<option value="Mr">Mr</option>
+						<option value="Mrs">Mrs</option>
+						<option value="Miss">Miss</option>
+						<option value="Sir">Sir</option>
+						<option value="Dr">Dr</option>
+						<option value="Fr">Fr</option>
+					</select>
+				</div>
+				<div class="col-5">
+					<label>Forename</label>
+					<input type="text">
+				</div>
+				<div class="col-5">
+					<label>Surname</label>
+					<input type="text">
+				</div>
+			</div>
+
+		</section>
+	</div>
+
+	<div class="col-12">
+		<section data-id="grouptwo">
+			<div class="section-header">
+				<div class="row">
+					<div class="col-6">2nd Group / Room</div>
+					<div class="col-6 text-right">2 Adults</div>
+				</div>
+			</div>
+
+			<div class="row form-row">
+				<div class="col-2">
+					<label for="">Title</label>
+					<select name="" id="">
+						<option value="">-</option>
+						<option value="Mr">Mr</option>
+						<option value="Mrs">Mrs</option>
+						<option value="Miss">Miss</option>
+						<option value="Sir">Sir</option>
+						<option value="Dr">Dr</option>
+						<option value="Fr">Fr</option>
+					</select>
+				</div>
+				<div class="col-5">
+					<label>Forename</label>
+					<input type="text">
+				</div>
+				<div class="col-5">
+					<label>Surname</label>
+					<input type="text">
+				</div>
+			</div>
+
+		</section>
+	</div>
+	<div class="col-12 text-right">
+		<button>Continue</button>
+	</div>
+</div>
