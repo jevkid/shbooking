063401453dc9f0f310c19496c39f6b4b4a802349
Megan Jevin <megan.jevin@inspiretec.com>
2018-05-21 08:07:33 +0100

    Started README to make handover easier

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# shbooking
+# Shbookings
+
+----
+## Navigation
+* Found in `nav.php` in partial_views
+* Each step/page has a few properties associated with it- `data-step`, which is a numeric value, `data-next`, which specifies the next page i.e. "details" or "accom", `data-prev`, which is the current page i.e. "party" or "travel", and `data-summary-id` which allows the summary to track which bits to show, i.e "air".
+* The continue button is a single button found in `index.php`- this has a property called `data-current-step` which is defaulted to 1.
+* The actual navigation bar is a list, and each step has a `data-nav-step` associated with it, i.e. "party" or "travel"
+    * The JS for step/page navigation is found in `scripts.js` and has been commented inline. When the continue button is selected, we loop through the partial containers and find the one which does *not* have the `hidden` class- we set this to the "current" page.
+    * The function takes an argument `isBackButton` to determine if we are going forwards in the journey or backwards.
+    * Regardless of which direction, we then hide the current step. Based on whether or not `isBackButton` is true or false, we will either increment or decrement the navigation, removing the `hidden` class from the target container.
+    * We then remove the `active` class from the current navigation state, and add it to the target navigation.
+    * If `isBackButton` is true and the current step is the 2nd one, we'll hide the back button. 
+
+----
+## Your Party
+
+----
+## Joining Points
+
+----
+## Coach
+
+---
+## Air
+
+----
+## Accommodation
+
+----
+## Guest Details
+
+----
+## Extras
+
+----
+## Confirm and Pay
+
+----
+## Confirmation Page
+
+
+
--- index.php
+++ index.php
@@ -8,77 +8,75 @@
 		<title>Booking Engine | Shearings</title>
 	</head>
 	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="pull-left">
+						<img src="https://d28wrjgfs2j02n.cloudfront.net/images/default-source/test-library/logo672568c0c1694119b999e3fd165ca7ad.jpg?sfvrsn=454c2974_1" width="176" style="padding: 1rem 0;" alt="">
+					</div>
+					<div class="pull-right text-right" style="padding: 1rem 0;">
+						<span style="color: #666666">Call us today on:</span>
+						<h3 style="color: #0068b9">01942 417545</h3>
+					</div>
+				</div>
+			</div>
+		</div>
 		
-	
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				<div class="pull-left">
-					<img src="https://d28wrjgfs2j02n.cloudfront.net/images/default-source/test-library/logo672568c0c1694119b999e3fd165ca7ad.jpg?sfvrsn=454c2974_1" width="176" style="padding: 1rem 0;" alt="">
+		<?php include("partial_views/nav.php"); ?>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-8">
+					<div data-step="1" data-next="travel" data-prev="party" data-summary-id="party">
+						<?php include("partial_views/yourParty-1.php"); ?>
+					</div>
+					<div class="hidden" data-step="2" data-next="travel" data-prev="travel" data-summary-id="joining">
+						<?php include("partial_views/joiningPoint-2.php"); ?>
+					</div>
+					<div class="hidden" data-step="3" data-next="travel" data-prev="travel" data-summary-id="seating">
+						<?php include("partial_views/coach-3.php"); ?>
+					</div>
+					<div class="hidden" data-step="4" data-next="accom" data-prev="travel" data-summary-id="air">
+						<?php include("partial_views/air-4.php"); ?>
+					</div>
+					<div class="hidden" data-step="5" data-next="details" data-prev="accom" data-summary-id="accom">
+						<?php include("partial_views/accommodation-5.php"); ?>
+					</div>
+					<div class="hidden" data-step="6" data-next="details" data-prev="accom-part-2" data-summary-id="accom-part-2">
+						<?php include("partial_views/accommodation-6.php"); ?>
+					</div>
+					<div class="hidden" data-step="7" data-next="extras" data-prev="accom-part-2">
+						<?php include("partial_views/guestDetails-7.php"); ?>
+					</div>
+					<div class="hidden" data-step="8" data-next="confirm" data-prev="extras" data-summary-id="extras">
+						<?php include("partial_views/extras-8.php"); ?>
+					</div>
+					<div class="hidden" data-step="9" data-prev="confirm">
+						<?php include("partial_views/payment-9.php"); ?>
+					</div>
+					<div class="pull-right">
+						<button type="button" data-current-step="1">Continue</button>
+					</div>
+					<div class="pull-left">
+						<a href="#" class="back-button hidden" data-previous-step>Back</a>
+					</div>
 				</div>
-				<div class="pull-right text-right" style="padding: 1rem 0;">
-					<span style="color: #666666">Call us today on:</span>
-					<h3 style="color: #0068b9">01942 417545</h3>
+				<div class="col-12 col-md-4">
+					<?php include("partial_views/summary.php"); ?>
 				</div>
 			</div>
 		</div>
-	</div>
-	
-	<?php include("partial_views/nav.php"); ?>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-12 col-md-8">
-				<div data-step="1" data-next="travel" data-prev="party" data-summary-id="party">
-					<?php include("partial_views/yourParty-1.php"); ?>
-				</div>
-				<div class="hidden" data-step="2" data-next="travel" data-prev="travel" data-summary-id="joining">
-					<?php include("partial_views/joiningPoint-2.php"); ?>
-				</div>
-				<div class="hidden" data-step="3" data-next="travel" data-prev="travel" data-summary-id="seating">
-					<?php include("partial_views/coach-3.php"); ?>
-				</div>
-				<div class="hidden" data-step="4" data-next="accom" data-prev="travel" data-summary-id="air">
-					<?php include("partial_views/air-4.php"); ?>
-				</div>
-				<div class="hidden" data-step="5" data-next="details" data-prev="accom" data-summary-id="accom">
-					<?php include("partial_views/accommodation-5.php"); ?>
-				</div>
-				<div class="hidden" data-step="6" data-next="details" data-prev="accom-part-2" data-summary-id="accom-part-2">
-					<?php include("partial_views/accommodation-6.php"); ?>
-				</div>
-				<div class="hidden" data-step="7" data-next="extras" data-prev="accom-part-2">
-					<?php include("partial_views/guestDetails-7.php"); ?>
-				</div>
-				<div class="hidden" data-step="8" data-next="confirm" data-prev="extras" data-summary-id="extras">
-					<?php include("partial_views/extras-8.php"); ?>
-				</div>
-				<div class="hidden" data-step="9" data-prev="confirm">
-					<?php include("partial_views/payment-9.php"); ?>
-				</div>
-				<div class="pull-right">
-					<button type="button" data-current-step="1">Continue</button>
-				</div>
-				<div class="pull-left">
-					<a href="#" class="back-button hidden" data-previous-step>Back</a>
-				</div>
-			</div>
-			<div class="col-12 col-md-4">
-				<?php include("partial_views/summary.php"); ?>
-			</div>
-		</div>
-	</div>
-
-	<?php include("partial_views/footer.php"); ?>
+		<?php include("partial_views/footer.php"); ?>
 
 
-	<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/jquery-nice-select@1.1.0/js/jquery.nice-select.min.js"></script>
-	<script src="dist/js/scripts.js"></script>
-	<script src="dist/js/flights.js"></script>
-	<script src="dist/js/coach.js"></script>
-	<script src="dist/js/room-cabin-info.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/jquery-nice-select@1.1.0/js/jquery.nice-select.min.js"></script>
+		<script src="dist/js/scripts.js"></script>
+		<script src="dist/js/flights.js"></script>
+		<script src="dist/js/coach.js"></script>
+		<script src="dist/js/room-cabin-info.js"></script>
 	</body>
 </html>
