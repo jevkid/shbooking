5c8ea2e1613e85676ea3f3d2ab68cd434bdca88f
Megan Jevin <megan.jevin@inspiretec.com>
2018-05-22 15:26:52 +0100

    Ability to add multiple coaches

--- dist/js/coach.js
+++ dist/js/coach.js
@@ -1,92 +1,107 @@
 function init(){
     var coachInput = document.getElementById('HolidayCoaches').getAttribute('value').replace(/'/g, '"');
     coachInput = JSON.parse(coachInput);
-    var componentId = coachInput[0].ComponentID;
-
-    var coaches = coachInput[0].SeatMapDetails;
-    coaches.sort(function(a, b){
-      return a.SeatId - b.SeatId;
-    });
-
     var numPax = document.getElementById('TotalPassengers').getAttribute('value');
-    var numSeats = coaches.length / 4;
-  
-    var count = 0;
+    var pax, componentId, coaches;
     var selectedSeats = [];
-    var pax;
+    var input, seat, summary, text, summaryText;
+    var count = [];
+  
+    for(var i = 0; i < coachInput.length; i++){
+      componentId = coachInput[i].ComponentID;
+      coaches = coachInput[i].SeatMapDetails;
+      coaches.sort(function(a, b){
+        return a.SeatId - b.SeatId;
+      });
 
-    var availMap = {0: "Available", 1: "Unavailable", 2: "Toilet", 3: "Booked", 4: "Reserved"};
+      var numSeats = coaches.length / 4;
   
-    for(var i = 1; i <= numSeats; i++){
-      var li = document.createElement('li');
-      var text = document.createTextNode(i);
-      li.appendChild(text);
-      li.setAttribute("class", "seatMap");
-      document.getElementById('coachMap').appendChild(li);
-    }
-  
-    for(var key in coaches){
-      if(coaches.hasOwnProperty(key)){
+      var availMap = {0: "Available", 1: "Unavailable", 2: "Toilet", 3: "Booked", 4: "Reserved"};
+    
+      for(var j = 1; j <= numSeats; j++){
         var li = document.createElement('li');
-        var liClass = availMap[coaches[key].Status].toLowerCase();
-        li.setAttribute("id", coaches[key].SeatNumber);
-        li.setAttribute("data-coach-id", componentId);
-        li.setAttribute("data-seat-id", coaches[key].SeatId);
-        li.setAttribute("data-seat-num", coaches[key].SeatNumber);
-        li.setAttribute("class", liClass);
-        
-        li.addEventListener('click', function(){
-          pax = document.querySelector('[data-coach-pax]').getAttribute('data-coach-pax');
-          pax = pax.split(',');
-          count++;
-          if(count <= numPax) {
-            $('[data-pax-name]').text(pax[count]);
-            document.querySelector('[data-pax-count]').removeAttribute('class', 'hidden');
-            selectedSeats.push({
-              coach: this.getAttribute('data-coach-id'), 
-              seat: this.getAttribute('data-seat-id')
-            });
-            var input = document.createElement('input');
-            input.setAttribute('type', 'hidden');
-            input.setAttribute('value', (this.getAttribute('data-coach-id') + ',' + this.getAttribute('data-seat-id')));
+        var text = document.createTextNode(j);
+        li.appendChild(text);
+        li.setAttribute("class", "seatMap");
+        document.getElementById('coachMap-' + (i + 1)).appendChild(li);
+      }
 
-            var seat = document.createElement('li');
-            var summary = document.createElement('li');
-            seat.setAttribute('class', 'seat');
-
-            var text = document.createTextNode(pax[count-1] + ': ' + this.getAttribute('data-seat-num'));
-            var summaryText = document.createTextNode(pax[count-1] + ': ' + this.getAttribute('data-seat-num'));
-            
-            seat.appendChild(text);
-            summary.appendChild(summaryText);
-            document.getElementById('customerMap').appendChild(seat);
-            document.getElementById('customerMap').appendChild(input);
-            document.getElementById('seatSummary').appendChild(summary);
-
-            this.removeAttribute("class", "available");
-            this.setAttribute("class", "selected");
+      for(var key in coaches){
+        var coachNum = (i + 1);
+        if(coaches.hasOwnProperty(key)){
+          var li = document.createElement('li');
+          var liClass = availMap[coaches[key].Status].toLowerCase();
+          // Add attributes to each "seat" to grab the data from them later on
+          li.setAttribute("id", coaches[key].SeatNumber);
+          li.setAttribute("data-coach-id", componentId);
+          li.setAttribute("data-seat-id", coaches[key].SeatId);
+          li.setAttribute("data-seat-num", coaches[key].SeatNumber);
+          // Store the coach count on each seat
+          li.setAttribute("data-count", coachNum);
+          li.setAttribute("class", liClass);
+          
+          li.addEventListener('click', function(){
+            // This grabs the names of the passengers from the group summary
+            pax = document.querySelector('[data-coach-pax]').getAttribute('data-coach-pax');
+            pax = pax.split(',');
+            // Gets which coach to use
+            var coachCount = this.getAttribute('data-count');
+            // Counts how many customers have been added to the coach already
+            var count = $('#customerMap-' + coachCount).children('li').length;
+            if(count < numPax) {
+              // Grabs the passenger name and appends it to the "Your Seats" bit
+              $('[data-pax-name]').text(pax[count + 1]);
+              // Show the "Your Seats" bit for the correct coach
+              $('#customerSelect-' + coachCount).removeClass('hidden');
+              // Store the selected eat information
+              selectedSeats.push({
+                coach: this.getAttribute('data-coach-id'), 
+                seat: this.getAttribute('data-seat-id')
+              });
+              // Create the hidden input
+              input = document.createElement('input');
+              input.setAttribute('type', 'hidden');
+              input.setAttribute('value', (this.getAttribute('data-coach-id') + ',' + this.getAttribute('data-seat-id')));
+              
+              seat = document.createElement('li');
+              summary = document.createElement('li');
+              seat.setAttribute('class', 'seat');
   
-            /* 
-             TODO:
-             Add additional look for selected element on hover
-             Click to remove element
-            */
+              text = document.createTextNode(pax[count] + ': ' + this.getAttribute('data-seat-num'));
+              summaryText = document.createTextNode(pax[count] + ': ' + this.getAttribute('data-seat-num'));
+              
+              seat.appendChild(text);
+              summary.appendChild(summaryText);
+              document.getElementById('customerMap-' + coachCount).appendChild(seat);
+              document.getElementById('customerMap-' + coachCount).appendChild(input);
+              document.getElementById('seatSummary').appendChild(summary);
+  
+              this.removeAttribute("class", "available");
+              this.setAttribute("class", "selected");
+    
+              /* 
+               TODO:
+               Add additional look for selected element on hover
+               Click to remove element
+              */
+            }
+            if(count >= numPax) {
+              $('[data-pax-name]').parents('p').addClass('hidden');
+            }
+          });
+
+          if(coaches[key].SeatNumber.indexOf('A') > -1) {
+            document.getElementById('coachMapA-' + (i + 1)).appendChild(li);
           }
-          if(count >= numPax) {
-            $('[data-pax-name]').parents('p').addClass('hidden');
+          if(coaches[key].SeatNumber.indexOf('B') > -1) {
+            document.getElementById('coachMapB-' + (i + 1)).appendChild(li);
           }
-        });
-        if(coaches[key].SeatNumber.indexOf('A') > -1) {
-          document.getElementById('coachMapA').appendChild(li);
-        }
-        if(coaches[key].SeatNumber.indexOf('B') > -1) {
-          document.getElementById('coachMapB').appendChild(li);
-        }
-        if(coaches[key].SeatNumber.indexOf('C') > -1) {
-          document.getElementById('coachMapC').appendChild(li);
-        }
-        if(coaches[key].SeatNumber.indexOf('D') > -1) {
-          document.getElementById('coachMapD').appendChild(li);
+          if(coaches[key].SeatNumber.indexOf('C') > -1) {
+            document.getElementById('coachMapC-' + (i + 1)).appendChild(li);
+          }
+          if(coaches[key].SeatNumber.indexOf('D') > -1) {
+            document.getElementById('coachMapD-' + (i + 1)).appendChild(li);
+          }
         }
       }
     }
