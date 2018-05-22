7beff2192c9dc319072a8850362397fb17d0bc42
cwallermedia <admin@cwallermedia.co.uk>
2018-05-22 17:18:17 +0100

    remove console log

--- dist/js/get-feeder-pickups.js
+++ dist/js/get-feeder-pickups.js
@@ -2,7 +2,6 @@
  var parse = JSON.parse(secondinput);
 
 $.each(parse[0].PickupPoints, function(i, item){
-    console.log(item.Name);
     $("#pickupfromselect").append('<option data-value="' + item.PickupPointID + '">' + item.Name + ' (' + item.Address + ') </option>').niceSelect('update');
     $("#dropoffselect").append('<option data-value="' + item.PickupPointID + '">' + item.Name + ' (' + item.Address + ') </option>').niceSelect('update');
 });
